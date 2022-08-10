76b8651eda44e73a01cd7f8398b0c1feb536c19b
jwAndroid <cjd9408er@naver.com>
2022-08-10 22:01:02 +0900

    Add name

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <p>Hello World!</p>
+        <p>최지웅</p>
       </header>
     </div>
   );
