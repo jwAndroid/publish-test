0606d2a0906fd9e6665452cefc965704d74b5dd7
jwAndroid <cjd9408er@naver.com>
2022-09-26 00:52:28 +0900

    Update emtion

---
+++ src/emotion.d.ts
@@ -0,0 +1,12 @@
+import '@emotion/react';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    color: {
+      black: string;
+      gray: string;
+      red: string;
+      orange: string;
+    };
+  }
+}
